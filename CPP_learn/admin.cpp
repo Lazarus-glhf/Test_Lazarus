087d01a16ef5fae205b4efde089a71393eb3655c
Lazarus-glhf <76900515+Lazarus-glhf@users.noreply.github.com>
2021-01-27 20:00:27 +0800

    Add files via upload

--- CPP_learn/admin.cpp
+++ CPP_learn/admin.cpp
@@ -4,32 +4,38 @@ using namespace std;
 class Admin //创建一个类，记录用户名，备注
 {
 public:
-    void setAdmin(char Name[]);//记录用户名
-    void setNotes(char Note[]);//记录备注
-    void printAdmin();//打印信息
+    void setAdmin(char Name[]); //记录用户名
+    void setNotes(char Note[]); //记录备注
+    void printAdmin();          //打印信息
 
 private:
-    char Admin_name[20], Note[40];//定义变量
+    char Admin_name[20], Note[40]; //定义变量
 };
 
 //函数实现
-void Admin::setAdmin(char name[20])
+void Admin::setAdmin(char name[])
 {
-    int i=0;
-    char name;
-    for(i=0;i<20;i++){
-
-        name[i]=name;
+    int i;
+    for (i = 0; i < 20; i++)
+    {
+        if(name[i]=='\0') break;
+        Admin_name[i]=name[i];
     }
 }
 void Admin::setNotes(char note1[])
 {
-    Note[] = note1;
+    int i=0;
+    for (i = 0; i < 40; i++)
+    {
+        cout<<note1[i];
+        if(note1[i]=='\0') break;
+        Note[i]==note1[i];
+    }
 }
 void Admin::printAdmin()
 {
-    cout <<"Admin:"<< Admin_name << endl
-        <<"Notes:"<< Note << endl;
+    cout << "Admin:" << Admin_name << endl
+        << "Notes:" << Note << endl;
 }
 
 //balabala
@@ -40,9 +46,9 @@ int main(void)
     cin >> name;
     cout << "Please input your notes:" << endl;
     cin >> note1;
-    cout<<endl;
+    cout << endl;
 
-    cout<<"pls check ur infomations"<<endl;
+    cout << "pls check ur infomations" << endl;
 
     Admin firstAdmin;
     firstAdmin.setAdmin(name);
